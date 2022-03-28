23939564f285aacbd9fad452a17ad315a978a6b2
David Jorge <david.jorge@appconcept.es>
2022-03-28 16:45:01 +0200

    add ERC1155 to run some tests

---
+++ ethereum/contracts/Kluest1155.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+// import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+
+contract Kluest1155 is ERC1155, ERC1155Burnable {
+
+}
