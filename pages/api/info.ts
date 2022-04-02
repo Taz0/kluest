83fb13feae5ec4a4788e2230bf4f806f11066b27
David Jorge <david.jorge@appconcept.es>
2022-04-02 13:47:30 +0200

    compile for production, add info endpoint to get info about the contract

--- pages/api/info.ts
+++ pages/api/info.ts
@@ -2,7 +2,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  result: boolean
+  contract: string,
+  key: string
 }
 
 export default function handler(
@@ -12,5 +14,10 @@ export default function handler(
   console.dir(process.env);
   console.log(`Contract ${process.env.TOKEN_CONTRACT_ADDRESS!}`);
   console.log(`Using mnemonic ${process.env.MNEMONIC?.substring(0, 5) || 'Unknown'}`);
-  res.status(200).json({ name: 'Hello world!' })
+  res.status(200).json(
+    { 
+      result: true,
+      contract: 'process.env.TOKEN_CONTRACT_ADDRESS',
+      key: process.env.MNEMONIC?.substring(0, 5) || 'Unknown'
+  })
 }
